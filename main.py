696cde1464535370ebeaaed0aad122b3914e58e1
Joe I <jingenito@vcssoftware.com>
2022-02-26 13:40:02 -0700

    adding pickle test for reference

--- main.py
+++ main.py
@@ -1,30 +1,17 @@
+from TypeModule import ScalingRatio as SRatio, FractalScalingType as fsType
 from SimilarFractalString import SelfSimilarFractalString as SSFS
-from TypeModule import FractalScalingType as fsType, ScalingRatio as SRatio
-import FractalImages
+import Util.SerializationModule as SerMod
 
-print('Beginning Fractal String Testbench.')
+print('Creating test data.')
+data = [SRatio(fsType.Ratio, 1.0/3.0),
+        SRatio(fsType.Gap, 1.0/3.0),
+        SRatio(fsType.Ratio, 1.0/3.0)]
+cString = SSFS(1000,4,data)
 
-print('Starting to build the Cantor String with 2 iterations.')
-filename = 'Images/CantorString2_1000.png'
-value = 1.0/3.0
-ScalingRatios = [SRatio(fsType.Ratio, value),
-                 SRatio(fsType.Gap, value),
-                 SRatio(fsType.Ratio, value)]
-fractalString = SSFS(1000, 2, ScalingRatios)
-print('Finished building Cantor String... Saving image.')
-FractalImages.CreateFractalStringImage(fractalString, (450,550), filename)
-print('Cantor String complete.')
-
-print("Beginning next fractal string test.")
-filename = "Images/TestString2_1000.png"
-value = 1.0/4.0
-ScalingRatios = [SRatio(fsType.Ratio, value),
-                 SRatio(fsType.Gap, value),
-                 SRatio(fsType.Ratio, value),
-                 SRatio(fsType.Gap, value)]
-fractalString = SSFS(1000, 2, ScalingRatios)
-print('Finished building Test String... Saving image.')
-FractalImages.CreateFractalStringImage(fractalString, (450,550), filename)
-print('Test String complete.')
-
-print('Testbench complete.')
+print('Finished creating data, now test pickling data.')
+filename = 'bin/PickleTest'
+SerMod.PickleData(cString,filename)
+print('Pickled data, now unpickling data.')
+reloaded_string = SerMod.UnpickleData(filename)
+print("Unpickled data, test Resolution, Tier : " + str(reloaded_string.Resolution) + ', ' + str(reloaded_string.Tier))
+print('Finished test.')
